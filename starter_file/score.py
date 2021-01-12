20d6a00efd157fe8dc73c86dd5f9e0d8e8e3b048
Gabi Lopez <30606674+Gabilopez1@users.noreply.github.com>
2021-01-12 02:24:12 +0100

    Update score.py

--- starter_file/score.py
+++ starter_file/score.py
@@ -15,21 +15,25 @@
 # +
 import json
 import pandas as pd
+import numpy as np
 import os
 import joblib, pickle
 from azureml.core import Model
 
 
-def init():
-    global daone
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best-automl-run.pkl')
-    daone = joblib.load(model_path)
 
+def init():
+    global model 
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best-automl-run.pkl')
+    print("Found model:", os.path.isfile(model_path)) #To check whether the model is actually present on the location we are looking at
+    model = joblib.load(model_path)
+   
 def run(data):
     try:
-        trynn = json.loads(data)
-        data = pd.DataFrame(trynn['data'])
-        result = daone.predict(data)
+        #data = np.array(json.loads(data))
+        data = json.loads(data)['data']
+        data = pd.DataFrame.from_dict(data)
+        result = model.predict(data)
         # You can return any data type, as long as it is JSON serializable.
         return result.tolist()
     except Exception as e:
