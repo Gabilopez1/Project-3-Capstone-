7503eab1b45c8996ee9485614f4158eaf87f087d
Gabi Lopez <30606674+Gabilopez1@users.noreply.github.com>
2021-01-14 01:04:47 +0000

    Add files via upload

--- starter_file/score.py
+++ starter_file/score.py
@@ -24,7 +24,7 @@ from azureml.core import Model
 
 def init():
     global model 
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best-automl-run.pkl')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_wine.pkl')
     print("Found model:", os.path.isfile(model_path)) #To check whether the model is actually present on the location we are looking at
     model = joblib.load(model_path)
    
